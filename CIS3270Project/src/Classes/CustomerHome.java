c2b3b2c98d8765866ce9cdba2d94b0bac9736785
henrycsal <henrycsal@gmail.com>
2015-05-02 17:10:40 -0400

    2 New Classes

---
+++ CIS3270Project/src/Classes/CustomerHome.java
@@ -0,0 +1,31 @@
+package Classes;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class CustomerHome extends JFrame{
+	public CustomerHome(){
+		JPanel p1 = new JPanel();
+		JPanel p2 = new JPanel();
+		p1.add(new JButton("Search flight system"));
+		p1.add(new JButton("View/Delete Registered Flights"));
+		p2.add(new JButton("Edit Personal Information"));
+		p2.add(new JButton("Logout"));
+		add(p1, BorderLayout.CENTER);
+		add(p2, BorderLayout.SOUTH);
+		
+	}
+
+	public static void main(String[] args){
+		CustomerHome frame = new CustomerHome();
+		frame.setTitle("Customer Page");
+		frame.setSize(500, 500);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+}
